b9b701f7edfe56c6b023076493a8389c2c8d2ce9
Sravya <sravyachandrapati16@gmail.com>
2024-08-16 20:07:13 +1200

    Initial Commit

---
+++ CardStore/CardStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardStoreApp.swift
+//  CardStore
+//
+//  Created by Sravya Chandrapati on 16/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CardStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
