b800bd2d3349545d5ccdd935d75af2472005e3c0
Sravya <sravyachandrapati16@gmail.com>
2024-08-17 12:05:15 +1200

    Card Model and a Json mockup file created

--- CardStore/Model/CardModel.swift
+++ CardStore/Model/CardModel.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+import Foundation
+
+struct Card: Identifiable, Codable {
+    let id: Int
+    let uid: String
+    let creditCardNumber: String
+    let creditCardExpiryDate: String
+    let creditCardType: String
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case uid
+        case creditCardNumber = "credit_card_number"
+        case creditCardExpiryDate = "credit_card_expiry_date"
+        case creditCardType = "credit_card_type"
+    }
+}
+
